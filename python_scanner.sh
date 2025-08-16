addcd22d9c074bd5ff071f969670d90d79ac5502
Uttej-Afk <111152327+Uttej-Afk@users.noreply.github.com>
2025-08-16 15:53:52 -0500

    Update python_scanner.sh

--- python_scanner.sh
+++ python_scanner.sh
@@ -2,8 +2,8 @@
 # Secure Login Node Python Process Monitor
 
 
-LOG_FILE="/var/tmp/$ProcessLog_$(date '+%y-%m-%d').log"
-CSV_FILE="/var/tmp/$ProcessCSV_$(date '+%y-%m-%d').csv"
+LOG_FILE="/var/tmp/ProcessLog_$(date '+%y-%m-%d').log"
+CSV_FILE="/var/tmp/ProcessCSV_$(date '+%y-%m-%d').csv"
 WHITELIST_USERS=("root" "slurm" "admin")
 WHITELIST_PROCS=("bash" "zsh" "sshd" "vim" "nano" "squeue" "sinfo" "code-server" "slurmd" "agetty" "systemd")
 
